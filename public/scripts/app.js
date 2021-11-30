447f608a604e899a45e93f9da1e0bbbf9ab196d2
Muaad Ahmed <muaad.abdi.ahmed@gmail.com>
2021-11-30 19:01:49 +0000

    Add increment to order modal

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -28,25 +28,4 @@ $(document).ready(() => {
   $('#navbarCollapse').on('click',function() {
     $('#navbar').toggleClass('active');
   });
-
-  $(".order-item").on("click", function() {
-    console.log("order this item, open the modal");
-    $("#order-modal").modal('show');
-  });
-
-  $('#modal-btn-increase').on('click', () => {
-    let currentVal = $(".form-control").val();
-    $(".form-control").val(Number(currentVal) + 1);
-  });
-
-  $('#modal-btn-decrease').on('click', () => {
-    let currentVal = $(".form-control").val();
-    if (currentVal > 0) {
-      $(".form-control").val(Number(currentVal) - 1);
-    }
-  });
-
-  $('.close').on('click', () => {
-    $("#order-modal").modal('hide');
-  })
 });
--- public/scripts/restaurant-client.js
+++ public/scripts/restaurant-client.js
@@ -1,13 +1,17 @@
 // Client facing scripts here
-const createResItem = (name, description) => {
-  let html = `<tr>
-  <th>
-    ${name}
-  </th>
-  <td>
-    ${description}
-  </td>
-</tr>`;
+const createResItem = (dishName, dishImg, dishPrice, dishDescription) => {
+  console.log(dishImg);
+  let html = `<div class="col">
+  <div class="order-item card shadow-sm flex-row">
+    <img width="50%" src="/img/${dishImg}">
+
+    <div class="card-body d-flex flex-column">
+      <h3 class="card-title">${dishName}</h3>
+      <p class="card-text">${dishDescription}</p>
+        <small class="text-muted text-end">$${dishPrice}</small>
+    </div>
+  </div>
+</div>`;
   return html;
 };
 
@@ -23,4 +27,25 @@ $(document).ready(() => {
     .then(res => {
       renderRes(res);
     });
+
+    $(".order-item").on("click", function() {
+      console.log("order this item, open the modal");
+      $("#order-modal").modal('show');
+    });
+
+    $('#modal-btn-increase').on('click', () => {
+      let currentVal = $(".form-control").val();
+      $(".form-control").val(Number(currentVal) + 1);
+    });
+
+    $('#modal-btn-decrease').on('click', () => {
+      let currentVal = $(".form-control").val();
+      if (currentVal > 0) {
+        $(".form-control").val(Number(currentVal) - 1);
+      }
+    });
+
+    $('.close').on('click', () => {
+      $("#order-modal").modal('hide');
+    })
 });
