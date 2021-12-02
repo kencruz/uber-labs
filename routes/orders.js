aeb3158ab5bf8ca862f4f4b8ea79a99e5dcb4c6e
Ken Cruz <ken@kencruz.ca>
2021-12-02 08:02:50 -0500

    added order updating and status checking routes

--- routes/orders.js
+++ routes/orders.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router  = express.Router();
 
-module.exports = (db) => {
+
+const getOrders = (db) => {
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM restaurant_order;`)
       .then(data => {
@@ -16,3 +17,40 @@ module.exports = (db) => {
   });
   return router;
 };
+
+const sendOrder = (db) => {
+  router.post("/", (req, res) => {
+    const orderId  = req.session.orderId;
+
+    // this will simulate a restaurant receiving an order and make it ready in
+    // 5 seconds
+    setTimeout(
+      db.query(`UPDATE restaurant_order
+        SET is_ready = TRUE 
+        WHERE id = ${orderId};`)
+        .catch(err => {
+          console.log(err);
+        }),
+      5000
+    );
+  });
+  return router;
+};
+
+const getOrderStatus = (db) => {
+  router.get("/status", (req, res) => {
+    const orderId  = req.session.orderId;
+    
+    db.query(`SELECT * FROM restaurant_order WHERE id = ${orderId};`)
+      .then(data => {
+        const restaurantOrder = data.rows[0];
+        res.json({ restaurantOrder });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+  return router;
+};
+
+module.exports = {getOrders, sendOrder, getOrderStatus};
